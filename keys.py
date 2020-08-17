25fad900c713adc1ea4472afd6365ed10c59ef33
eCrimeLabs <rand@ecrime.dk>
2020-08-17 20:12:51 +0200

    Update keys.py

--- keys.py
+++ keys.py
@@ -6,7 +6,7 @@ misp_key = '<MISP_API_KEY>'
 misp_verifycert = True
 misp_tags = ['tlp:amber', 'misp-galaxy:financial-fraud="Phishing"']
 
-misp_distribution = 0 # 0 = Organisation only, 1 = This community only, 2 = Connected communities, 3 = All communities, 4 = Sharing Group
+misp_distribution = 3 # 0 = Organisation only, 1 = This community only, 2 = Connected communities, 3 = All communities, 4 = Sharing Group
 sharing_group_id = 1 # Only to be used in combination with misp_distribution = 4
 
 misp_threat_level_id = 1 # 0 = Undefined, 1 = Low, 2 = Medium, 3 = High
